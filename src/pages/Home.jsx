3ff458f8dd4871333addba47851c99e27391a68c
Muhammadali00000 <muhammadalinarov@gmail.com>
2025-01-08 16:28:28 +0500

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Hero from '../components/Hero/Hero'
+import Strategy from '../components/Strategy/Strategy'
+import Product from '../components/Product/Product'
+import Activity from '../components/Activity/Activity'
+import Modern from '../components/Modern/Modern'
+import Power from '../components/Power/Power'
+import Social from '../components/Social/Social'
+import Media from '../components/Media/Media'
+
+
+
+
+
+const Home = () => {
+  return (
+    <>
+    <Hero/>
+    <Strategy/>
+    <Product/>
+    <Activity/>
+    <Modern/>
+    <Power/>
+    <Social/>
+    <Media/>
+    </>
+  )
+}
+
+export default Home
